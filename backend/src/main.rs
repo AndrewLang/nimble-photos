cfdddef0f5539a8ab71a07a8944071e862b35e8c
Lang Yu Xin <lang.xin+f8u@montel.energy>
2026-01-26 22:39:45 -0700

    Update Cargo.toml and main.rs for nimble-photos initialization and logging setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use nimble_web::*;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    init_logging();
+
+    log::info!("Start building application...");
+    let mut builder = AppBuilder::new();
+
+    log::info!("Starting application...");
+    let app = builder.build();
+    app.start().await?;
+
+    Ok(())
+}
+
+fn init_logging() {
+    let mut builder = env_logger::Builder::from_default_env();
+    builder.filter_level(log::LevelFilter::Debug);
+    let _ = builder.try_init();
 }
